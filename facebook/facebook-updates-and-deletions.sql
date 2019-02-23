79cf1d986f2395c844e2c34e6a97bc23be05f0d3
tanrsahn <46093101+tanrsahn@users.noreply.github.com>
2019-02-23 20:45:26 +0300

    Create facebook-updates-and-deletions.sql

---
+++ facebook/facebook-updates-and-deletions.sql
@@ -0,0 +1,9 @@
+USE facebook;
+SET SQL_SAFE_UPDATES = 0;
+UPDATE f_user SET activeness=false WHERE user_id=4001;
+UPDATE application SET application_name="facebook" WHERE application_name="fcebook";
+UPDATE f_page SET username="monica42" WHERE username="monica";
+
+DELETE FROM f_like WHERE profile_id=3001;
+DELETE FROM likeable_object WHERE likeable_object_id=6006;
+DELETE FROM other_accounts WHERE profile_id=3001;
